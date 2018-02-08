112a11000587f9cd16460143370222a20431f0a2
suzan baert <suzan.baert@outlook.com>
2018-02-08 21:51:06 +0100

    clean script

--- 01_Reading_pdf.R
+++ 01_Reading_pdf.R
@@ -19,94 +19,28 @@ indented_text <- pdf_script %>%
   str_extract_all(pattern = "\r\n\\s{5,}.+") %>%
   unlist()
 
+#this works but it splits the phrases:
 clean_indented_text <- indented_text %>%
   str_replace("(END OF )?COLD OPEN", "") %>%  #removes cold open and end of cold open text
-  str_replace("ACT [A-Z]+", "") %>%  #removes text as ACT ONE
-  str_replace("\\(.+\\)", "") #%>%  #removes anything between brackets
-  #str_replace("\r\n\\s+", "")  #removes the new line and extra spaces
+  str_replace("(END OF )?ACT [A-Z]+", "") %>%  #removes text as ACT ONE
+  str_replace("TAG", "") %>%
+  str_replace("END OF SHOW", "") %>%  
+  str_replace("\\(.+\\)", "") %>%  #removes anything between brackets
+  str_replace("\r\n\\s+", "")  #removes the new line and extra spaces
 
 
-  
-  #remains removes anything between brackes
-  
+#collapsed in one chunk
+one_long_text <- paste(clean_idented_text, collapse = " ")
 
 
-str_replace_all(script_quotes, "\r\n\\s+", "")
+#words quite well, but trips up add " I " and " I'm ", "I've "
+#str_view_all(one_long_text, "[A-Z]{3,}(\\s*[A-Z]?[a-z]+\\W*)+") 
 
+#works well
+#str_view_all(one_long_text, "[A-Z]{3,}(\\s*\\W*[A-Z]?\\W*[a-z]+\\W*\\d*)+")  
 
-str_view(script_quotes, "\\(.+\\)")
-
-
-test <- pdf_script[2]
-
-##THIS ONE WORKS: GETS ALL TALKING PEOPLE
-#looks for a single uppercase word that is alone on its line
-str_view_all(test, "\\\r\n\\s*[A-Z]{2,}\\\r\n")
-
-
-#identifying the indented lines - this removes the commentary lines such as "People shake their heads"
-str_view_all(test, "\\\r\n\\s{3,}.+")
-
-str_view(test, "(END OF )?COLD OPEN")
+str_extract_all(one_long_text, "[A-Z]{3,}(\\s*\\W*[A-Z]?\\W*[a-z]+\\W*\\d*)+")  
 
 
 
-
-test %>%
-  str_replace_all("\\\r\\n", "") %>%
-  str_view_all(" [A-Z]{2,}.+(?!TOM)")
-
-str_extract_all(test, " [A-Z]{2,}")
-str_split(test, " [A-Z]{2,}" )
-
-
-str_split(pdf_script, pattern)
-
-q <- "OLGA is een gans. KATRIEN is much smarter"
-str_which(q, "[A-Z]{2,}")
-
-
-str_view_all("bacaccad", "a(?!c{2,})")
-
-
-
-#I want to get chunks of text
-#this gives every ine
-script <- str_split(pdf_script, pattern = "\\\r\\n")
-
-unlist(script)
-
-
-
-
-#script gives a vector per page
-#Page 1 and 2 are unnecessary info so I will remove them
-pdf_script_no_intro <- pdf_script[3:34]
-test <- data.frame(text = pdf_script_no_intro)
-test <- mutate(text = cat(text))
-
-#converting to usual text and putting in a dataframe
-
-
-#there is a header on op of every page
-#PARKS AND RECREATION         “Harvest Festival”     [03009]    1.\r\n  1st Revised Blue Shooting Draft                     10/07/10
-script <- str_replace(script, "PARKS.+\\\r\\n.+10", "")
-
-
-#split vectors into chunks based on \r\n
-script <- str_split(script, pattern = "\\\r\\n")
-cat(script[[1]])
-
-
-#`not working et`
-
-str_view_all(script, "[A-Z]+")
-
-
-str_extract_all(script, "\\\r\\n\\s[A-Z]+\\s\\\r\\n")
-
-str_replace(script, "\\s+", "\\s")
-
-
-
-
+ 
